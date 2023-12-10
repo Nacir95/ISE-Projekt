ff49d51c1c12e29baa084943e5884d554038db2e
Ahmad1993Aj <ahmad1993aj@outlook.com>
2023-12-10 19:27:01 +0100

    ADD test.py file and ignore .idea file

---
+++ test.py
@@ -0,0 +1,13 @@
+print("Hi")
+
+from dataclasses import dataclass
+from pathlib import Path
+
+import os
+import urllib.request as request
+from zipfile import ZipFile
+import tensorflow as tf
+import time
+
+import numpy as np 
+
