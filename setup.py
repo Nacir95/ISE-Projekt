ae54e358a55788ea15c7699df2c0742f5d08dbda
na43085 <nacir.affes@studmail.w-hs.de>
2023-12-10 17:43:43 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "ISE-Projekt"
+AUTHOR_USER_NAME = "Nacir95"
+SRC_REPO = "Classification"
+AUTHOR_EMAIL = "naciraffes@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A project for Master module ISE, using CI/CD and AWS",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
