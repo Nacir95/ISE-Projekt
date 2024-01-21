4c3c3890b515ad1fcb3e888b6c0c15bb3c74e738
Ahmad1993Aj <ahmad1993aj@outlook.com>
2024-01-21 21:26:17 +0100

    ADD:tranierte Mode,Data,Fix:Error

--- src/Classification/pipeline/predict.py
+++ src/Classification/pipeline/predict.py
@@ -22,7 +22,7 @@ class PredictionPipeline:
         result = np.argmax(model.predict(test_image), axis=1)
         print(result)
 
-        if result[0] == 0:
+        if result[0] == 1:
             prediction = 'No'
             return [{ "image" : prediction}]
         else:
